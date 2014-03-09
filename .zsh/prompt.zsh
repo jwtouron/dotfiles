7b5b15b92f0a8d79dc5a55517c1d044544c2095e
John Touron <johntouron@gmail.com>
2014-03-09 11:36:22 -0500

    Changed git prompt info in zsh
    Based off of info at: http://blog.joshdick.net/2012/12/30/my_git_prompt_for_zsh.html

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,38 +1,61 @@
-function git_prompt_info {
-  local ref=$(=git symbolic-ref HEAD 2> /dev/null)
-  local gitst="$(=git status 2> /dev/null)"
-  local pairname=${${${GIT_AUTHOR_EMAIL#pair+}%@github.com}//+/\/}
-  if [[ ${pairname} == 'ch' || ${pairname} == '' ]]; then
-    pairname=''
-  else
-    pairname=" ($pairname)"
+GIT_PROMPT_PREFIX="${PR_BOLD_GREEN}[%{$reset_color%}"
+GIT_PROMPT_SUFFIX="${PR_BOLD_GREEN}]%{$reset_color%}"
+GIT_PROMPT_AHEAD="${PR_BOLD_RED}ANUM%{$reset_color%}"
+GIT_PROMPT_BEHIND="${PR_BOLD_CYAN}BNUM%{$reset_color%}"
+GIT_PROMPT_MERGING="${PR_BOLD_MAGENTA}⚡%{$reset_color%}"
+GIT_PROMPT_UNTRACKED="${PR_BOLD_RED}●%{$reset_color%}"
+GIT_PROMPT_MODIFIED="${PR_BOLD_YELLOW}●%{$reset_color%}"
+GIT_PROMPT_STAGED="${PR_BOLD_GREEN}●%{$reset_color%}"
+
+parse_git_branch() {
+  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
+}
+
+parse_git_state() {
+  local GIT_STATE=""
+
+  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$NUM_AHEAD" -gt 0 ]; then
+    GIT_STATE="$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD} "
   fi
 
-  if [[ -f .git/MERGE_HEAD ]]; then
-    if [[ ${gitst} =~ "unmerged" ]]; then
-      gitstatus=" $PR_BOLD_REDunmerged%{$reset_color%}"
-    else
-      gitstatus=" $PR_BOLD_GREENmerged%{$reset_color%}"
-    fi
-  elif [[ ${gitst} =~ "Changes to be committed" ]]; then
-    gitstatus=" $PR_BOLD_BLUE!%{$reset_color%}"
-  elif [[ ${gitst} =~ "use \"git add" ]]; then
-    gitstatus=" $PR_BOLD_RED!%{$reset_color%}"
-  elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-    gitstatus=" $PR_BOLD_YELLOW*%{$reset_color%}"
-  else
-    gitstatus=''
+  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
+  if [ "$NUM_BEHIND" -gt 0 ]; then
+    GIT_STATE="$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND} "
   fi
 
-  if [[ -n $ref ]]; then
-    echo "$PR_BOLD_GREEN/${ref#refs/heads/}%{$reset_color%}$gitstatus$pairname"
+  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
+  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
   fi
+
+  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+  fi
+
+  if ! git diff --quiet 2> /dev/null; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
+  fi
+
+  if ! git diff --cached --quiet 2> /dev/null; then
+    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+  fi
+
+  if [[ -n $GIT_STATE ]]; then
+    echo "$GIT_STATE"
+  fi
+
+}
+
+git_prompt_string() {
+  local git_where="$(parse_git_branch)"
+  [ -n "$git_where" ] && echo "$GIT_PROMPT_PREFIX$PR_YELLOW${git_where#(refs/heads/|tags/)} $(parse_git_state)$GIT_PROMPT_SUFFIX "
 }
 
 loc="$PR_BOLD_GREEN%n@%m%{$reset_color%}"
 dir="$PR_BOLD_CYAN%1~%{$reset_color%}"
 git="$(git_prompt_info)"
-PROMPT='$loc $dir $git$PR_BOLD_WHITE%#%{$reset_color%} '
+PROMPT='$loc $dir $(git_prompt_string)$PR_BOLD_WHITE%#%{$reset_color%} '
 
 check="$(echo -e "\xe2\x9c\x93")"
 cross="$(echo -e "\xe2\x9c\x97")"
