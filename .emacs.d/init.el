85a2fccfb0b6db21e7480c6f95ea75ec2261d545
John Touron <johntouron@gmail.com>
2014-03-24 03:45:39 +0100

    Fixed grammar error

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,7 +59,7 @@ Position the cursor at its beginning, according to the current mode."
 
 (defun smart-open-line-above ()
   "Insert an empty line above the current line.
-Position the cursor at it's beginning, according to the current mode."
+Position the cursor at its beginning, according to the current mode."
   (interactive)
   (move-end-of-line nil)
   (delete-horizontal-space)
