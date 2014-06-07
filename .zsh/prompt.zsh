62392970b79382260666d8e54a90259814aaed2d
John Touron <johntouron@gmail.com>
2014-06-07 03:40:40 +0100

    Tweaked zsh prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,3 +1,3 @@
-exitcode='%(?.${PR_BOLD_GREEN}√ $?.${PR_BOLD_RED}X $?)%{$reset_color%}'
+exitcode='%(?.${PR_BOLD_GREEN}√.${PR_BOLD_RED}X $?)%{$reset_color%}'
 dir="$PR_BOLD_CYAN%1~%{$reset_color%}"
 PROMPT="$exitcode $dir %# "
