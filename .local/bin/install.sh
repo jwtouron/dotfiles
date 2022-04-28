f270f5a15cb1ef4e5e1b76feec2211c040d75550
John Touron <johntouron@gmail.com>
2022-04-28 12:46:58 -0500

    update

---
+++ .local/bin/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install paccache cleanup hook
+
+command -v pacman >/dev/null || exit 0
+
+sudo mkdir -p /etc/pacman.d/hooks || exit 1
+
+cat << EOF | sudo tee /etc/pacman.d/hooks/paccache-cleanup.hook >/dev/null
+[Trigger]
+Operation = Upgrade
+Type = Package
+Target = *
+
+[Action]
+Description = Cleaning pacman cache...
+When = PostTransaction
+Exec = /usr/bin/paccache -r
+EOF
