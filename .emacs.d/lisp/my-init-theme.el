0215a0f3065f11f6959e2d9474aa8bee4147314e
John Touron <johntouron@gmail.com>
2022-07-27 09:23:37 -0500

    changes from thinkpad on Wed Jul 27 09:23:37 AM CDT 2022

--- .emacs.d/lisp/my-init-theme.el
+++ .emacs.d/lisp/my-init-theme.el
@@ -1,87 +1,69 @@
-(defmacro use-package-theme (&rest forms)
-  (let ((enabled (cl-find ':enabled forms :test 'equal))
-        (forms (cl-remove :enabled forms :test 'equal)))
-    )
-  ;;   `(cdr (quote ,forms))
-  ;; `(let* ((enabled (cl-find ':enabled forms :test 'equal))
-  ;;         (forms2 (cl-remove ':enabled forms :test 'equal)))
-  ;;    (use-package ,(car ,forms2))
-  ;;    ;; `(use-package (car ,forms2)
-  ;;    ;;   ,@(cdr forms2)
-  ;;    ;;   (when (not enabled)
-  ;;    ;;     ,@(cons ':if nil))
-  ;;    ;;   )
-  ;;    )
+(defmacro use-themes (enabled &rest themes)
+  (cl-labels ((apply-if-nil (theme)
+                            (if (equal enabled (car theme))
+                                theme
+                              (cons (car theme)
+                                    (append '(:if nil) (cdr theme))))))
+    (cons 'progn
+          (mapcar #'(lambda (theme)
+                      (cons 'use-package (apply-if-nil theme)))
+                  themes))))
+(put 'use-themes 'lisp-indent-function 1)
+
+(use-themes spaceway-theme
+  (arjen-grey-theme
+   :init (load-theme 'arjen-grey t)
+   :config (set-face-background hl-line-face "#2f4f4f"))
+
+  (atom-one-dark-theme
+   :init (load-theme 'atom-one-dark t))
+
+  (color-theme-sanityinc-tomorrow
+   :init (load-theme 'sanityinc-tomorrow-night t))
+
+  (gruber-darker-theme
+   :init (load-theme 'gruber-darker t))
+
+  (minimal-theme
+   :init (load-theme 'minimal t)
+   :config
+   (progn
+     (set-face-attribute 'region nil :background "grey40")
+     (set-face-attribute 'font-lock-comment-face nil :foreground "grey32")
+     (set-face-attribute 'font-lock-comment-delimiter-face nil :foreground "grey32")))
+
+  (modus-themes
+   :init (modus-themes-load-themes)
+   :config (modus-themes-load-vivendi))
+
+  (nord-theme
+   :init (load-theme 'nord t))
+
+  (spaceway-theme
+   :ensure nil
+   :init
+   (add-to-list 'custom-theme-load-path (concat user-emacs-directory "lisp/spaceway"))
+   (load-theme 'spaceway t))
+
+  (tao-theme
+   :custom (tao-theme-use-sepia nil)
+   :init (load-theme 'tao-yin t))
+
+  (tron-legacy-theme
+   :init
+   (setq tron-legacy-theme-vivid-cursor t
+         tron-legacy-theme-softer-bg t)
+   (load-theme 'tron-legacy t))
+
+  (vscode-dark-plus-theme
+   :init (load-theme 'vscode-dark-plus t))
+
+  (warm-night-theme
+   :init (load-theme 'warm-night t))
+
+  (zenburn-theme
+   :init (load-theme 'zenburn t))
   )
-(use-package-theme arjen-grey-theme
-                   :enabled
-                   :init (load-theme 'arjen-grey t)
-                   :config (set-face-background hl-line-face "#2f4f4f"))
-
-(use-package arjen-grey-theme
-  :if nil
-  :init (load-theme 'arjen-grey t)
-  :config (set-face-background hl-line-face "#2f4f4f"))
-
-(use-package atom-one-dark-theme
-  :init (load-theme 'atom-one-dark t))
-
-(use-package color-theme-sanityinc-tomorrow
-  :if nil
-  :init (load-theme 'sanityinc-tomorrow-night t))
-
-(use-package gruber-darker-theme
-  :if nil
-  :init (load-theme 'gruber-darker t))
-
-(use-package minimal-theme
-  :if nil
-  :init (load-theme 'minimal t)
-  :config
-  (progn
-    (set-face-attribute 'region nil :background "grey40")
-    (set-face-attribute 'font-lock-comment-face nil :foreground "grey32")
-    (set-face-attribute 'font-lock-comment-delimiter-face nil :foreground "grey32")))
-
-(use-package modus-themes
-  :if nil
-  :init (modus-themes-load-themes)
-  :config (modus-themes-load-vivendi))
-
-(use-package nord-theme
-  :if nil
-  :init (load-theme 'nord t))
-
-(use-package spaceway-theme
-  :if nil
-  :ensure nil
-  :init
-  (add-to-list 'custom-theme-load-path (concat user-emacs-directory "lisp/spaceway"))
-  (load-theme 'spaceway t))
-
-(use-package tao-theme
-  :if nil
-  :custom (tao-theme-use-sepia nil)
-  :init (load-theme 'tao-yin t))
-
-(use-package tron-legacy-theme
-  :if nil
-  :init
-  (setq tron-legacy-theme-vivid-cursor t
-        tron-legacy-theme-softer-bg t)
-  (load-theme 'tron-legacy t))
-
-(use-package vscode-dark-plus-theme
-  :if nil
-  :init (load-theme 'vscode-dark-plus t))
-
-(use-package warm-night-theme
-  :if nil
-  :init (load-theme 'warm-night t))
-
-(use-package zenburn-theme
-  :if nil
-  :init (load-theme 'zenburn t))
 
 (custom-set-faces
  '(show-paren-match ((t (:underline t :foreground nil :background nil)))))
