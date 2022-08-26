8476c85fbe1c881c927dbec0b07ce461ba72b5d9
John Touron <johntouron@gmail.com>
2022-08-26 14:25:43 -0500

    changes from john-hp-laptop on Fri Aug 26 02:25:43 PM CDT 2022

--- .local/bin/launch.sh
+++ .local/bin/launch.sh
@@ -28,7 +28,11 @@ File Manager
 HTop
 Volume"
 
-choice=$(echo "$options" | awk '{print NR ". " $0}' | dmenu -f -i -n -l 6 -c -bw 5 -p 'Launch:' | awk '{$1=""; print $0}')
+if command -v rofi >/dev/null; then
+    choice=$(echo "$options" | awk '{print NR ". " $0}' | rofi -dmenu -i -p 'Launch:' -case-sensitive -auto-select -theme ~/.config/rofi/rounded-nord-dark.rasi | awk '{$1=""; print $0}')
+else
+    choice=$(echo "$options" | awk '{print NR ". " $0}' | dmenu -f -i -n -l 6 -c -bw 5 -p 'Launch:' | awk '{$1=""; print $0}')
+fi
 
 case "$choice" in
     " Browser") browser ;;
