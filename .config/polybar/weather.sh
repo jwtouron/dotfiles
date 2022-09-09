26f0000ae481f1503264849b7825df0e73f55d1a
John Touron <johntouron@gmail.com>
2022-09-09 09:28:31 -0500

    changes from thinkpad-laptop on Fri Sep 9 09:28:31 AM CDT 2022

--- .config/polybar/weather.sh
+++ .config/polybar/weather.sh
@@ -7,5 +7,5 @@ if [ -n "$loc" ]; then
 fi
 
 #curl -Ss "https://wttr.in${loc}?0&T&Q" | cut -c 16- | head -2 | xargs echo "⛅"
-weather=$(curl -Ss "https://wttr.in${loc}?0&T&Q" 2>/dev/null | cut -c 16- | head -2 | xargs echo " ")
+weather=$(curl -Ssk "https://wttr.in${loc}?0&T&Q" 2>/dev/null | cut -c 16- | head -2 | xargs echo " ")
 echo " $weather "
