a87cd01c61a7497c1b7930a9d878b5bc6f00aa08
John Touron <johntouron@gmail.com>
2022-09-19 13:13:47 -0500

    changes from thinkpad-laptop on Mon Sep 19 01:13:47 PM CDT 2022

--- .config/stumpwm/startup-programs.lisp
+++ .config/stumpwm/startup-programs.lisp
@@ -6,7 +6,6 @@
   (run-shell-command "picom &")
   (run-shell-command "pgrep cbatticon || cbatticon &")
   ;; (run-shell-command "pgrep cbatticon || [ \"$(cbatticon -p | wc -l)\" -gt 1 ] && cbatticon &")
-  (run-shell-command "pgrep udiskie || udiskie; udiskie -s &")
 
   ;;(run-shell-command "pgrep polybar || for m in $(polybar --list-monitors | cut -d\":\" -f1); do MONITOR=$m polybar --reload stumpwm; done")
   )
