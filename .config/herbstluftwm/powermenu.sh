4b8a6e895a86ff0c95284fb894c4176593dd1fc6
John Touron <johntouron@gmail.com>
2022-11-29 10:00:43 -0600

    changes from void-vm on Tue Nov 29 10:00:43 AM CST 2022

--- .config/herbstluftwm/powermenu.sh
+++ .config/herbstluftwm/powermenu.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
+_poweroff() {
+    if command loginctl >/dev/null; then
+        loginctl poweroff
+    elif command systemctl >/dev/null; then
+        systemctl poweroff
+    fi
+}
+
+_reboot() {
+    if command loginctl >/dev/null; then
+        loginctl reboot
+    elif command systemctl >/dev/null; then
+        systemctl reboot
+    fi
+}
+
 _lock() {
     for lock in i3lock-fancy i3lock xlock; do
         if command -v "$lock" >/dev/null; then
@@ -16,6 +32,6 @@ case "$choice" in
     "Loc(k)") _lock ;;
     "Sus(p)end") _lock; systemctl suspend ;;
     "Lo(g)out") herbstclient quit ;;
-    "(R)eboot") systemctl reboot ;;
-    "Shutdo(w)n") systemctl poweroff ;;
+    "(R)eboot") _reboot ;;
+    "Shutdo(w)n") _poweroff ;;
 esac
