06d83633aac95d14656400a8a8a3b6f78150f253
John Touron <johntouron@gmail.com>
2022-12-04 10:58:57 -0600

    changes from john-hp-laptop on Sun Dec 4 10:58:55 AM CST 2022

--- .emacs.d/lisp/my-init-langs.el
+++ .emacs.d/lisp/my-init-langs.el
@@ -83,27 +83,11 @@
 
 (add-hook 'lisp-mode-hook #'paredit-mode)
 
-(use-package sly
-  :custom (sly-default-lisp 'roswell)
-  :hook (sly-mode . paredit-mode)
+(use-package slime
+  :hook (slime-repl-mode . paredit-mode)
   :init
-  (let ((ros-config (concat user-emacs-directory
-                            "ros-conf.lisp")))
-    (setq sly-lisp-implementations
-          `((sbcl    ("sbcl"))
-            (ccl     ("ccl"))
-            (clisp   ("clisp"))
-            (ecl     ("ecl"))
-            (roswell ("ros" "-Q" "-l" ,ros-config "run"))
-            ;; (qlot ("qlot" "exec" "ros" "run" "-S" "."))
-            ))))
-
-(use-package sly-asdf
-  :after sly
-  :init (add-to-list 'sly-contribs 'sly-asdf 'append))
-
-(use-package sly-quicklisp
-  :after sly)
+  (when (executable-find "sbcl")
+    (setq inferior-lisp-program "sbcl")))
 
 ;; lua
 
