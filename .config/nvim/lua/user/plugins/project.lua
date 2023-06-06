b6d27e293efbd2e7dd103c6d830d4f00c1a1228e
John Touron <johntouron@gmail.com>
2023-06-06 11:40:44 -0500

    project.nvim

---
+++ .config/nvim/lua/user/plugins/project.lua
@@ -0,0 +1,12 @@
+return {
+  "ahmedkhalf/project.nvim",
+  dependencies = "nvim-telescope/telescope.nvim",
+  event = "VeryLazy",
+  config = function()
+    require("project_nvim").setup()
+
+    local telescope = require("telescope")
+    telescope.load_extension("projects")
+    vim.keymap.set("n", "<leader>sp", function() telescope.extensions.projects.projects() end, { desc = "[P]rojects" })
+  end,
+}
