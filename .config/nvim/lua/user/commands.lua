d97f654b35d04f00b12f646bd17f3624bb5df7a0
John Touron <johntouron@gmail.com>
2024-04-30 09:06:57 -0500

    update

--- .config/nvim/lua/user/commands.lua
+++ .config/nvim/lua/user/commands.lua
@@ -3,3 +3,25 @@ vim.api.nvim_create_user_command(
   "read !date '+\\%Y-\\%m-\\%d'",
   { desc = "Insert the current date as YYYY-MM-DD below the current line." }
 )
+
+vim.api.nvim_create_user_command(
+  "Todos",
+  function(arg)
+    local dir = arg.args ~= '' and arg.args or '.'
+    local grepprg = vim.opt.grepprg:get()  -- save grepprg
+    local regex = '(FIXME\\|HACK\\|NOTE\\|TODO) *(\\([^)]*\\))? *:'
+    if vim.fn.executable("rg") == 1 then
+      vim.opt.grepprg = "rg --vimgrep '" .. regex .. "' " .. dir
+    else
+      vim.opt.grepprg = "grep -rnE '" .. regex .. "' " .. dir
+    end
+    local bang = arg.bang and '!' or ''
+    pcall(function() vim.cmd("grep" .. bang) end)
+    vim.opt.grepprg = grepprg  -- restore grepprg
+  end,
+  {
+    desc = "Find all FIXMEs, HACKs, NOTEs, and TODOs",
+    nargs = '?',
+    bang = true,
+  }
+)
