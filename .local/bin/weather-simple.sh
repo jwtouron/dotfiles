44a7911c18239fe1a3e3cb36f76ca210ede4e5d4
John Touron <johntouron@gmail.com>
2024-07-03 08:03:25 -0500

    changes from john-dell-laptop on Wed Jul 3 08:03:25 AM CDT 2024

--- .local/bin/weather-simple.sh
+++ .local/bin/weather-simple.sh
@@ -10,22 +10,48 @@ fi
 
 LOC="$(curl -s ipinfo.io | jq -r '.loc')"
 STATIONS_URL="$(curl -s https://api.weather.gov/points/$LOC | jq -r '.properties.observationStations')"
-STATION_URL="$(curl -s $STATIONS_URL | jq -r '.observationStations[0]')"
-WEATHER="$(curl -s $STATION_URL/observations/latest | jq -r '.properties')"
-DESC="$(echo $WEATHER | jq -r '.textDescription')"
-TEMP="$(echo $WEATHER | jq -r '.temperature')"
-HEAT_INDEX="$(echo $WEATHER | jq -r '.heatIndex')"
+STATIONS="$(curl -s $STATIONS_URL | jq -r '.observationStations[]')"
+for URL in $(echo "$STATIONS" | tr '\n' ' '); do
+    WEATHER="$(curl -s $URL/observations/latest | jq -r '.properties')"
+    if echo "$WEATHER" | jq -r 'select(.textDescription != null and .temperature != null and .heatIndex != null)' >/dev/null; then
+        DESC="$(echo $WEATHER | jq -r '.textDescription')"
+        TEMP="$(echo $WEATHER | jq -r '.temperature')"
+        HEAT_INDEX="$(echo $WEATHER | jq -r '.heatIndex')"
 
-if [ "$(echo $TEMP | jq -r '.unitCode')" = "wmoUnit:degC" ]; then
-    TEMP=$(echo "$(echo $TEMP | jq -r .value) * 1.8 + 32" | bc)
-else
-    TEMP="$(echo $TEMP | jq -r '.value')"
-fi
+        if [ "$(echo $TEMP | jq -r '.unitCode')" = "wmoUnit:degC" ]; then
+            TEMP=$(echo "$(echo $TEMP | jq -r .value) * 1.8 + 32" | bc)
+        else
+            TEMP="$(echo $TEMP | jq -r '.value')"
+        fi
 
-if [ "$(echo $HEAT_INDEX | jq -r '.unitCode')" = "wmoUnit:degC" ]; then
-    HEAT_INDEX=$(echo "$(echo $HEAT_INDEX | jq -r .value) * 1.8 + 32" | bc)
-else
-    HEAT_INDEX="$(echo $HEAT_INDEX | jq -r '.value')"
-fi
+        if [ "$(echo $HEAT_INDEX | jq -r '.unitCode')" = "wmoUnit:degC" ]; then
+            HEAT_INDEX=$(echo "$(echo $HEAT_INDEX | jq -r .value) * 1.8 + 32" | bc)
+        else
+            HEAT_INDEX="$(echo $HEAT_INDEX | jq -r '.value')"
+        fi
 
-printf "%s %.1f(%.1f)°F\n" "$DESC" "$TEMP" "$HEAT_INDEX"
+        printf "%s %.1f(%.1f)°F\n" "$DESC" "$TEMP" "$HEAT_INDEX"
+
+        break
+    fi
+done
+exit 0
+# STATION_URL="$(curl -s $STATIONS_URL | jq -r '.observationStations[0]')"
+# WEATHER="$(curl -s $STATION_URL/observations/latest | jq -r '.properties')"
+# DESC="$(echo $WEATHER | jq -r '.textDescription')"
+# TEMP="$(echo $WEATHER | jq -r '.temperature')"
+# HEAT_INDEX="$(echo $WEATHER | jq -r '.heatIndex')"
+#
+# if [ "$(echo $TEMP | jq -r '.unitCode')" = "wmoUnit:degC" ]; then
+#     TEMP=$(echo "$(echo $TEMP | jq -r .value) * 1.8 + 32" | bc)
+# else
+#     TEMP="$(echo $TEMP | jq -r '.value')"
+# fi
+#
+# if [ "$(echo $HEAT_INDEX | jq -r '.unitCode')" = "wmoUnit:degC" ]; then
+#     HEAT_INDEX=$(echo "$(echo $HEAT_INDEX | jq -r .value) * 1.8 + 32" | bc)
+# else
+#     HEAT_INDEX="$(echo $HEAT_INDEX | jq -r '.value')"
+# fi
+#
+# printf "%s %.1f(%.1f)°F\n" "$DESC" "$TEMP" "$HEAT_INDEX"
