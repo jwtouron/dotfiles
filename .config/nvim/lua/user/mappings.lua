abaa25fecd4c0181d0d3917edd9da537f1aebc0a
John Touron <john.touron@cymanii.org>
2025-10-27 12:59:18 -0500

    changes from RR184728IP01 on Mon Oct 27 12:59:18 CDT 2025

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -81,6 +81,12 @@ vim.keymap.set("x", "#", [[y?\V<C-R>=escape(@",'/\')<CR><CR>]])
 vim.keymap.set('t', '<esc>', [[<C-\><C-n>]])
 vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]])
 
+-- Make the arglist more like harpoon
+vim.keymap.set('n', '<M-h>', function() vim.cmd "silent 1argument" end)
+vim.keymap.set('n', '<M-j>', function() vim.cmd "silent 2argument" end)
+vim.keymap.set('n', '<M-k>', function() vim.cmd "silent 3argument" end)
+vim.keymap.set('n', '<M-l>', function() vim.cmd "silent 4argument" end)
+
 --------------------------------------------------------------------------------
 -- Tabs
 --------------------------------------------------------------------------------
