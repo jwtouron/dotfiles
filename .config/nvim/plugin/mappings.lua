c8b45ceb69e2f6ed6c121cd5ce40eb45eb21095c
John Touron <john.touron@cymanii.org>
2026-03-03 12:08:25 -0600

    changes from RR184728IP01 on Tue Mar 3 12:08:25 CST 2026

--- .config/nvim/plugin/mappings.lua
+++ .config/nvim/plugin/mappings.lua
@@ -51,5 +51,3 @@ vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window he
 vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
 vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
-vim.keymap.set("c", "<C-q>", "<Esc>q:k")
