2852ac979840340d237b477f0b8558702dda418d
John Touron <john.touron@cymanii.org>
2026-08-18 08:39:46 -0500

    changes from RR184728IP01 on Tue Aug 18 08:39:46 CDT 2026

--- .config/nvim/colors/mine.lua
+++ .config/nvim/colors/mine.lua
@@ -4,24 +4,25 @@ vim.cmd.highlight "clear"
 vim.g.colors_name = "mine"
 
 local c = {
-  black =      "#1d2328",
-  white =      "#e1e7e9",
-  -- light_gray = "#c0c8cc",
-  light_gray = "#b8c1c5",
-  -- light_gray = "#aeb8bd",
-  -- light_gray = "#a4afb4",
-  dark_gray =  "#30383e",
-  red =        "#d36f72",
-  green =      "#68c174",
-  yellow =     "#d0b66a",
-  blue =       "#5eacd3",
-  magenta =    "#b889d3",
-  cyan =       "#63c0ba",
-  maroon =     "#824a4f",
-  -- maroon =     "#7a464b",
-  -- maroon =     "#704146",
-  -- maroon =     "#663c41",
-  debug =      "#FF0000",
+  black =       "#1d2328",
+  white =       "#e1e7e9",
+  -- light_gray =  "#c0c8cc",
+  light_gray =  "#b8c1c5",
+  -- light_gray =  "#aeb8bd",
+  -- light_gray =  "#a4afb4",
+  medium_gray = "#747d82",
+  dark_gray =   "#30383e",
+  red =         "#d36f72",
+  green =       "#68c174",
+  yellow =      "#d0b66a",
+  blue =        "#5eacd3",
+  magenta =     "#b889d3",
+  cyan =        "#63c0ba",
+  maroon =      "#824a4f",
+  -- maroon =      "#7a464b",
+  -- maroon =      "#704146",
+  -- maroon =      "#663c41",
+  debug =       "#FF0000",
 }
 
 local function set(opts)
@@ -31,7 +32,7 @@ local function set(opts)
 end
 
 set { "Normal",    fg = c.white, bg = c.black }
-set { "Comment",   fg = c.maroon }
+set { "Comment",   fg = c.medium_gray }
 set { "Constant",  fg = c.magenta }
 set { "Number",    fg = c.blue }
 set { "Special",   fg = c.cyan }
@@ -74,10 +75,3 @@ set { "DiagnosticHint",  fg = c.cyan }
 set { "DiagnosticInfo",  fg = c.blue }
 set { "DiagnosticOk",    fg = c.green }
 set { "DiagnosticWarn",  fg = c.yellow }
-
--- Plugin-specific
-
-set { "OilEmpty",     fg = c.light_gray }
-set { "OilHidden",    fg = c.light_gray }
-set { "OilDir",       fg = c.white }
-set { "OilDirHidden", fg = c.light_gray }
